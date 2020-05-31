29fa1de0eff251667b2438d63d412c92f1764cf7
Ömer Günal <o.gunal977@gmail.com>
2020-05-31 18:33:11 +0300

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -2,14 +2,13 @@ import os
 from ast import literal_eval
 from collections import Counter 
 
+
 def find_groups(techniques,softwares):
+	
+	
 	f = open("groupInfo.txt", "r")
 	data = f.read()
 	data = literal_eval(data)
-
-	#techniques = input("Techniques used (ID or Name) (Seperate with comma):")
-	#softwares = input("Softwares used (ID or Name) (Seperate with comma):")
-
 	techniques = techniques.split(",")
 	softwares = softwares.split(",")
 	rate = {}
@@ -19,15 +18,12 @@ def find_groups(techniques,softwares):
 		for technique in techniques:
 			for group_technique in data[group]["techniques"]:
 				if technique.lower() in group_technique.lower():
-#					print(technique)
 					rate[group] += 1
 		for software in softwares:
 			for group_technique in data[group]["techniques"]:
 				if software.lower() in group_technique.lower():
-#					print(software)
 					rate[group] += 1
 
 	result = Counter(rate)
 	result = result.most_common(5)
-
 	return(result)
